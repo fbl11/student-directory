363c41cd1dcd022482d1d12b458d75501e8aa381
Freya <freyab_london@yahoo.co.uk>
2018-08-21 15:46:00 +0000

    Adds solution to Exercise 6 - format print with 'string'.center()

--- directory.rb
+++ directory.rb
@@ -1,49 +1,32 @@
-# Exercise 5
-# Our code only works with the student name and cohort. Add more information:
-# hobbies, country of birth, height, etc.
-
 def input_students
   students = []
-  name = "placeholder"
-
+  puts 'Please enter the names of the students'
+  puts 'To finish, just hit return twice'
+  name = gets.chomp
+  
   while !name.empty? do
-    puts 'Please enter the name of the student'
-    puts 'To finish, just hit return'
-    name = gets.chomp.capitalize
-
-    break if name.empty? == true
-
-    # not used until Exercise 7
-    # puts 'Please enter the student cohort'
-    # cohort = gets.chomp.to_sym
-    
-    puts 'Please enter the student\'s hobby'
-    hobby = gets.chomp.capitalize
-
-    puts 'Please enter the student\'s hight (in cm)'
-    height = gets.chomp
-    
-    students << {name: name, cohort: :november, hobby: hobby, height: height}
-    puts "Now we have #{students.count} students\n\n"
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students".center(40, '*')
+    name = gets.chomp
   end
-    students
+  students
 end
 
+# Exercise 6
+# Research how the method center() of the String class works. Use it in your code to make the output beautifully aligned.
 def print_header
-  puts 'The students of Villains Academy'
-  puts '-------------' 
+  puts 'The students of Villains Academy'.center(40, '*')
+  puts '-------------'.center(40) 
 end
 
 def print_names(names)
     names.each do |student|
-      puts "#{student[:name]} (#{student[:cohort]} cohort)"
-      puts "Hobby: #{student[:hobby]}"
-      puts "Height: #{student[:height]}\n\n"
+      puts "#{student[:name]} (#{student[:cohort]} cohort)".center(40)
     end
 end
 
 def print_footer(number_of)
-    puts "Overall, we have #{number_of.count} great students"
+    puts "Overall, we have #{number_of.count} great students".center(40, '*')
 end
 
 students = input_students
