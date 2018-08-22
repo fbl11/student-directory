dee2236267297f5251800a0fed66a57d41ee904e
Freya <freyab_london@yahoo.co.uk>
2018-08-22 10:27:47 +0000

    Adds solution to Exercise 9 - use singular/plural forms correctly when printing out number of students

--- directory.rb
+++ directory.rb
@@ -1,61 +1,53 @@
-# Exercise8
+# Exercise9
+# Right now if we have only one student, the user will see a message "Now we have 1 students", whereas it should be "Now we have 1 student".
+# How can you fix it so that it used singular form when appropriate and plural form otherwise?
 
-students = [
-    {name: 'Dr. Hannibal Lecter', cohort: :august},
-    {name: 'Darth Vader', cohort: :november},
-    {name: 'Nurse Ratched', cohort: :august},
-    {name: 'Michael Corleone', cohort: :november},
-    {name: 'Alex DeLarge', cohort: :april},
-    {name: 'The Wicked Witch of the West', cohort: :november},
-    {name: 'Terminator', cohort: :november},
-    {name: 'Freddy Krueger', cohort: :november},
-    {name: 'The Joker', cohort: :november},
-    {name: 'Joffrey Baratheon', cohort: :april},
-    {name: 'Norman Bates', cohort: :november}
-]
-# not used for this exercise
-# def input_students
-#   students = []
-#   puts 'Please enter the names of the students'
-#   puts 'To finish, just hit return twice'
-#   name = gets.chomp
+# students = [
+#     {name: 'Dr. Hannibal Lecter', cohort: :august},
+#     {name: 'Darth Vader', cohort: :november},
+#     {name: 'Nurse Ratched', cohort: :august},
+#     {name: 'Michael Corleone', cohort: :november},
+#     {name: 'Alex DeLarge', cohort: :april},
+#     {name: 'The Wicked Witch of the West', cohort: :november},
+#     {name: 'Terminator', cohort: :november},
+#     {name: 'Freddy Krueger', cohort: :november},
+#     {name: 'The Joker', cohort: :november},
+#     {name: 'Joffrey Baratheon', cohort: :april},
+#     {name: 'Norman Bates', cohort: :november}
+# ]
+
+def input_students
+  students = []
+  puts 'Please enter the names of the students'
+  puts 'To finish, just hit return twice'
+  name = gets.chomp
   
-#   while !name.empty? do
-#     students << {name: name, cohort: :november}
-#     puts "Now we have #{students.count} students"
-#     name = gets.chomp
-#   end
-#   students
-# end
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+# How can you fix it so that it used singular form when appropriate and plural form otherwise?
+    students.length == 1 ? (puts "Now we have #{students.count} student")
+    : (puts "Now we have #{students.count} students")
+    name = gets.chomp
+  end
+  students
+end
 
 def print_header
   puts 'The students of Villains Academy'
   puts '-------------' 
 end
 
-# change the way the users are displayed: print them grouped by cohorts. 
 def print_names(names)
-# collects all unique existing cohort values
-  cohort_values = names.map {|student_entry| student_entry.values[1]}.uniq
-  puts cohort_values
-
-# iterates over all unique months collected in 'cohort_values'
-  cohort_values.each do |month|
-# iterates over all students recorded in 'students' array
     names.each do |student|
-# compares current cohort_value to current student's cohort value and if equal puts full student entry
-      if student[:cohort] == month
-        puts "#{student[:name]} (#{student[:cohort]} cohort)"
-      end 
+      puts "#{student[:name]} (#{student[:cohort]} cohort)"
     end
-  end
-  
 end
 
 def print_footer(number_of)
-  puts "Overall, we have #{number_of.count} great students"
+    puts "Overall, we have #{number_of.count} great students"
 end
 
+students = input_students
 print_header
 print_names(students)
 print_footer(students)
