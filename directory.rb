33e6c023d401924a1a7af388b05b1ca46c7ed867
Freya <freyab_london@yahoo.co.uk>
2018-08-22 09:26:21 +0000

    Adds solution to Exercise 7 - provides default value for user input and converts string to symbol

--- directory.rb
+++ directory.rb
@@ -1,32 +1,48 @@
+# Exercise7
+# In the input_students method the cohort value is hard-coded.
+# What if the user makes a typo? Answer: Downcase/upcase/capitalize can ensure correct capitalisation
+# other typos would need to be checked for case by case.  For cohort, input would be expected to be
+# one of twelve months.  Verification could be done via dictionary lookup that compares user input with 
+# values in dictionary and sets to closest match (matches first three letters for example)
+
 def input_students
   students = []
-  puts 'Please enter the names of the students'
-  puts 'To finish, just hit return twice'
-  name = gets.chomp
-  
-  while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students".center(40, '*')
-    name = gets.chomp
+  name = "placeholder"
+
+  while name != "exit" do
+    puts 'Please enter the name of the student'
+    puts 'To finish, type Exit'
+    name = gets.chomp.capitalize
+
+    break if name == "Exit"
+
+# How can you ask for both the name and the cohort? 
+    puts 'Please enter the student cohort'
+# The input will be given to you as a string? How will you convert it to a symbol?
+    cohort = gets.chomp.to_sym
+    if cohort.empty?
+      cohort = :default
+    end
+    
+    students << {name: name, cohort: cohort}
+    puts "Now we have #{students.count} students\n\n"
   end
-  students
+    students
 end
 
-# Exercise 6
-# Research how the method center() of the String class works. Use it in your code to make the output beautifully aligned.
 def print_header
-  puts 'The students of Villains Academy'.center(40, '*')
-  puts '-------------'.center(40) 
+  puts 'The students of Villains Academy'
+  puts '-------------' 
 end
 
 def print_names(names)
-    names.each do |student|
-      puts "#{student[:name]} (#{student[:cohort]} cohort)".center(40)
-    end
+  names.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
 end
 
 def print_footer(number_of)
-    puts "Overall, we have #{number_of.count} great students".center(40, '*')
+    puts "Overall, we have #{number_of.count} great students"
 end
 
 students = input_students
